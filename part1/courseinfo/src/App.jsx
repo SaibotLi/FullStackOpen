2ba44928a8ad49a25b761708907a58dc62795472
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-05-05 22:14:35 -0300

    Solución inicial pero aún no completa

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,28 @@
+import Header from "./Header.jsx"
+import Content from "./Content.jsx"
+import Total from "./Total.jsx"
+
+const App = () => {
+  return (
+    <div> 
+<Header />
+<Content />
+<Total />
+    </div>
+  )
+}
+
+export default App
+
+
+/* 
+const App = () => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content ... />
+      <Total ... />
+    </div>
+  )
+} 
+*/
---
+++ part1/courseinfo/src/Header.jsx
@@ -0,0 +1,8 @@
+const course = 'Half Stack application development'
+
+const Header = () => {  
+    return(
+    <h1>{course}</h1>
+    )
+}
+export default Header
